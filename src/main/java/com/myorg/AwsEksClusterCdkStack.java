50d699b2fb58d2f0002928949995dc465bee7633
Arjen Wassink <awassink@quintor.nl>
2026-01-29 22:58:57 +0100

    Step 4: Build your CDK app Step 5: List the CDK stacks in your app Step 6: Define your Lambda function Step 7: Define your Lambda function URL Step 8: Synthesize a CloudFormation template

--- src/main/java/com/myorg/AwsEksClusterCdkStack.java
+++ src/main/java/com/myorg/AwsEksClusterCdkStack.java
@@ -1,10 +1,11 @@
 package com.myorg;
 
+import software.amazon.awscdk.CfnOutput;
+import software.amazon.awscdk.services.lambda.*;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class AwsEksClusterCdkStack extends Stack {
     public AwsEksClusterCdkStack(final Construct scope, final String id) {
@@ -16,9 +17,27 @@ public class AwsEksClusterCdkStack extends Stack {
 
         // The code that defines your stack goes here
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsEksClusterCdkQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        // Define the Lambda function resource
+        Function myFunction = Function.Builder.create(this, "HelloWorldFunction")
+                .runtime(Runtime.NODEJS_20_X) // Provide any supported Node.js runtime
+                .handler("index.handler")
+                .code(Code.fromInline(
+                        "exports.handler = async function(event) {" +
+                                " return {" +
+                                " statusCode: 200," +
+                                " body: JSON.stringify('Hello World!')" +
+                                " };" +
+                                "};"))
+                .build();
+
+        // Define the Lambda function URL resource
+        FunctionUrl myFunctionUrl = myFunction.addFunctionUrl(FunctionUrlOptions.builder()
+                .authType(FunctionUrlAuthType.NONE)
+                .build());
+
+        // Define a CloudFormation output for your URL
+        CfnOutput.Builder.create(this, "myFunctionUrlOutput")
+                .value(myFunctionUrl.getUrl())
+                .build();
     }
 }
