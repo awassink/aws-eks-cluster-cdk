01905e386d9353f4ecc377a1039d848be00a5770
Arjen Wassink <awassink@quintor.nl>
2026-01-30 17:13:33 +0100

    Changed EKS masterRole to OrganizationAccountAccessRole for standard kubectl access.

--- src/main/java/com/myorg/AwsEksClusterCdkStack.java
+++ src/main/java/com/myorg/AwsEksClusterCdkStack.java
@@ -5,6 +5,7 @@ import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.eks.KubernetesVersion;
 import software.amazon.awscdk.services.iam.AccountRootPrincipal;
+import software.amazon.awscdk.services.iam.IRole;
 import software.amazon.awscdk.services.iam.Role;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
@@ -24,12 +25,13 @@ public class AwsEksClusterCdkStack extends Stack {
 
         Role clusterAdmin =
                 Role.Builder.create(this, "cluster-admin").assumedBy(new AccountRootPrincipal()).build();
+        IRole accountAdmin = Role.fromRoleArn(this, "account-admin", "arn:aws:iam::607241929434:role/OrganizationAccountAccessRole");
 
         // Define EKS Cluster with ManagedNodeGroup
         Cluster ngpEksCluster = Cluster.Builder.create(this, "NgpEksCluster")
                 .version(KubernetesVersion.V1_32)
                 .kubectlLayer(new KubectlV32Layer(this,"kubectl"))
-                .mastersRole(clusterAdmin)
+                .mastersRole(accountAdmin)
                 .defaultCapacity(1)
                 .defaultCapacityInstance(InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM))
                 .build();
