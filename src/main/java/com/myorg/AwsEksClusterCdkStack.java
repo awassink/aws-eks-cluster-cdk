e57eec6d2dde509c4edf8d7aa3bc4eb3e377da12
Arjen Wassink <awassink@quintor.nl>
2026-01-30 15:53:04 +0100

    Added EKS masterRole for kubectl access.

--- src/main/java/com/myorg/AwsEksClusterCdkStack.java
+++ src/main/java/com/myorg/AwsEksClusterCdkStack.java
@@ -4,6 +4,8 @@ import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
 import software.amazon.awscdk.services.eks.KubernetesVersion;
+import software.amazon.awscdk.services.iam.AccountRootPrincipal;
+import software.amazon.awscdk.services.iam.Role;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
@@ -20,10 +22,14 @@ public class AwsEksClusterCdkStack extends Stack {
 
         // The code that defines your stack goes here
 
+        Role clusterAdmin =
+                Role.Builder.create(this, "cluster-admin").assumedBy(new AccountRootPrincipal()).build();
+
         // Define EKS Cluster with ManagedNodeGroup
         Cluster ngpEksCluster = Cluster.Builder.create(this, "NgpEksCluster")
                 .version(KubernetesVersion.V1_32)
                 .kubectlLayer(new KubectlV32Layer(this,"kubectl"))
+                .mastersRole(clusterAdmin)
                 .defaultCapacity(1)
                 .defaultCapacityInstance(InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM))
                 .build();
